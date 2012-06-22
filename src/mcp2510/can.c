da3fd55ed367725423ac6741f5eb2559e7bd90df
Andriy Lin <lxk19921205@gmail.com>
2012-06-22 20:34:39 +0800

    comment on can.c && can_sensor.c, but confused about their relationship, TODO in can.c can_read...

--- src/mcp2510/can.c
+++ src/mcp2510/can.c
@@ -10,7 +10,7 @@
 #include<linux/devfs_fs_kernel.h> 
 #include<asm/io.h>
 #include<asm/uaccess.h>
-  
+
 #include<linux/time.h>
 #include<linux/timer.h>
 
@@ -48,28 +48,29 @@ void inttimer_register(struct timer_list *pdata,unsigned long timeover);
 
 //mcp2510 send an interrupt to s3c2410,and the reason caused interrupt should analysis.
 //triggered the interrupt on low level edge.NOW WE USE INTERRUPT 2
+//注：用于之后的request_irq()，应该是中断的回调函数
 void can_interrupt(int irq,void *d,struct pt_regs *regs)
 {	
 	unsigned char buffer,tmp;
 
 	disable_irq(IRQ_EINT2);
-       
+
 	//Write_2510(CANINTE, 0x00); //close interrupt in mcp2510 //it is not true ,and 
 	//will induce message lost. luo 20110401
 
 	printk("in can interrupting---1--\n");
-	
-	buffer = Read_Instr_2510(CANINTF);	
-/*	
-#define MERRF 0X80
-#define WAKIF 0X40
-#define ERRIF 0X20
-#define TX2IF 0X10
-#define TX1IF 0X08
-#define TX0IF 0X04
-#define RX1IF 0X02
-#define RX0IF 0X01
-*/
+
+	buffer = Read_Instr_2510(CANINTF);
+	/*
+	#define MERRF 0X80
+	#define WAKIF 0X40
+	#define ERRIF 0X20
+	#define TX2IF 0X10
+	#define TX1IF 0X08
+	#define TX0IF 0X04
+	#define RX1IF 0X02
+	#define RX0IF 0X01
+	*/
 	if((buffer&MERRF) >0)
 	{
 		printk("mcp2510 message error\n");
@@ -88,12 +89,12 @@ void can_interrupt(int irq,void *d,struct pt_regs *regs)
 			printk("TX2 transmit error\n");
 		if((tmp&MLOA)>0)
 			printk("TX2 transmit lost arbitration\n");
-		BitModify_Instr_2510(CANINTF, MERRF, 0);		
+		BitModify_Instr_2510(CANINTF, MERRF, 0);
 	}
-    if((buffer&WAKIF) >0)
+	if((buffer&WAKIF) >0)
 	{
 		printk("mcp2510 wake up\n");
-		BitModify_Instr_2510(CANINTF, WAKIF, 0);		
+		BitModify_Instr_2510(CANINTF, WAKIF, 0);
 	}
 	if((buffer&ERRIF) >0)
 	{
@@ -107,72 +108,71 @@ void can_interrupt(int irq,void *d,struct pt_regs *regs)
 		if((tmp&TXWAR)>0) printk("Transmit Error Count >=96\n");
 		if((tmp&RXWAR)>0) printk("Receive Error Count >=96\n");
 		if((tmp&EWARN)>0) printk("Receive Error Count >=96 or Transmit Error Count >=96\n");
-		BitModify_Instr_2510(CANINTF, ERRIF, 0);	
+		BitModify_Instr_2510(CANINTF, ERRIF, 0);
 	}
 	if((buffer&TX2IF) >0)
 	{
-		printk("mcp2510 TX2 BUFFER BECOMING EMPTY\n");	
-		BitModify_Instr_2510(CANINTF, TX2IF, 0);	
+		printk("mcp2510 TX2 BUFFER BECOMING EMPTY\n");
+		BitModify_Instr_2510(CANINTF, TX2IF, 0);
 	}
 	if((buffer&TX1IF) >0)
 	{
-		printk("mcp2510 TX1 BUFFER BECOMING EMPTY\n");	
-		BitModify_Instr_2510(CANINTF, TX1IF, 0);	
+		printk("mcp2510 TX1 BUFFER BECOMING EMPTY\n");
+		BitModify_Instr_2510(CANINTF, TX1IF, 0);
 	}
 	if((buffer&TX0IF) >0)
 	{
-		printk("mcp2510 TX0 BUFFER BECOMING EMPTY\n");	
-		BitModify_Instr_2510(CANINTF, TX0IF, 0);	
+		printk("mcp2510 TX0 BUFFER BECOMING EMPTY\n");
+		BitModify_Instr_2510(CANINTF, TX0IF, 0);
 	}
 	if((buffer&RX1IF)>0)
 	{
-		printk("mcp2510 RX1 BUFFER BECOMING FULL\n");	
+		printk("mcp2510 RX1 BUFFER BECOMING FULL\n");
 		can_data_receive(1);
 		BitModify_Instr_2510(CANINTF, RX1IF, 0);
 		kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
 	}
-    if((buffer&RX0IF)>0)
+	if((buffer&RX0IF)>0)
 	{
-		printk("mcp2510 RX0 BUFFER BECOMING FULL\n");	
+		printk("mcp2510 RX0 BUFFER BECOMING FULL\n");
 		can_data_receive(0);
 		BitModify_Instr_2510(CANINTF, RX0IF, 0);
 		kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
-	}  	
-	
-	SRCPND &= (~0x00000002);    
-   	INTPND = INTPND;
-	
-	enable_irq(IRQ_EINT2);	   			
+	}
 
+	SRCPND &= (~0x00000002);
+	INTPND = INTPND;
+
+	enable_irq(IRQ_EINT2);
 }
 
+//注：计时器到时候了
 void inttimer_timeover(unsigned long arg)
 {
-	
 	//unsigned char buffer;
 	printk("\n===============In timer--ing==================\n");
 
-//	buffer = ReadStatus_Instr_2510();
+	//	buffer = ReadStatus_Instr_2510();
 
-//	printk("Now can state is %x\n",buffer);
-	
-/*	if((buffer&RX0IF)>0)
+	//	printk("Now can state is %x\n",buffer);
+
+	/*	if((buffer&RX0IF)>0)
 	{
-		 can_data_receive(0);
-		BitModify_Instr_2510(CANINTF, RX0IF, 0);
-                kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
+	can_data_receive(0);
+	BitModify_Instr_2510(CANINTF, RX0IF, 0);
+	kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
 	}
 	else if((buffer&RX1IF)>0)
 	{
-		 can_data_receive(1);
-		BitModify_Instr_2510(CANINTF, RX1IF, 0);
-                kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
+	can_data_receive(1);
+	BitModify_Instr_2510(CANINTF, RX1IF, 0);
+	kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
 	}
-*/
+	*/
 	inttimer_register(int_timer,TIME_STEP);
-	      
 }
 
+//注：计时器的注册
 void inttimer_register(struct timer_list *pdata,unsigned long timeover)
 {
 	init_timer(pdata);
@@ -183,75 +183,79 @@ void inttimer_register(struct timer_list *pdata,unsigned long timeover)
 }
 
 
+/************************************************************************/
+/* 用于之后的candev_fops，此所谓一切皆文件                              */
+/************************************************************************/
+
 int can_open(struct inode *inode,struct file *filp)
 {
 	int flags;
 
-	MOD_INC_USE_COUNT;	
+	MOD_INC_USE_COUNT;
 
 	local_irq_save(flags);
 	Init_SPI();
 	Init_MCP2510();
 	local_irq_restore(flags);
 
-       /********************set timer******************************************************/
-        int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
-    if(int_timer==NULL)return -1;
-        memset(int_timer,0,sizeof(struct timer_list));
-        inttimer_register(int_timer,TIME_STEP);
+	/********************set timer******************************************************/
+	int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
+	if(int_timer==NULL)return -1;
+	memset(int_timer,0,sizeof(struct timer_list));
+	inttimer_register(int_timer,TIME_STEP);
 
-   /*
+	/*
 	spi_tx_data(0xff);
 	spi_tx_data(0xff);
 	spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
-    spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
+	spi_tx_data(0xff);
 	*/
-    return 0;
+	return 0;
 }
 
 static int can_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsigned long arg)
 {
 	unsigned char tmp;
-	
+
 	switch(cmd)
 	{
 	case IOCTL_MOD_SET:
 		copy_from_user(&tmp,(unsigned char *)arg,1);
 		printk("============IOCTL MODE SET===============\n");
 		switch(tmp){
-			case OP_NORMAL:
-				MCP2510_SetNormalMode();
-				return 1;
-			case OP_LISTEN_ONLY:
-				MCP2510_SetListenOnlyMode();
-				return 1;
-			case OP_LOOPBACK:
-				MCP2510_SetLoopbackMode();
-				return 1;
-			case OP_SLEEP:
-				MCP2510_SetSleepMode();
-				return 1;
-			case OP_CONFIG:
-				MCP2510_SetConfigMode();
-				return 1;
-			default:
-				return -1;
+		case OP_NORMAL:
+			MCP2510_SetNormalMode();
+			return 1;
+		case OP_LISTEN_ONLY:
+			MCP2510_SetListenOnlyMode();
+			return 1;
+		case OP_LOOPBACK:
+			MCP2510_SetLoopbackMode();
+			return 1;
+		case OP_SLEEP:
+			MCP2510_SetSleepMode();
+			return 1;
+		case OP_CONFIG:
+			MCP2510_SetConfigMode();
+			return 1;
+		default:
+			return -1;
 		}
 		break;
 	case IOCTL_GET_MODE:
-                if(!arg)
+		if(!arg)
 			return -1;
-	
+
 		printk("============IOCTL GET MODE===============\n");
 		tmp= Read_Instr_2510(CANSTAT);
 		tmp=((tmp&0xe0)>>1);
@@ -259,14 +263,14 @@ static int can_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsi
 		break;
 	case IOCTL_GET_CANSTAT:
 		if(!arg)
-                        return -1;
+			return -1;
 
 		printk("============IOCTL GET CANSTAT===============\n");
 		tmp= ReadStatus_Instr_2510();
 		copy_to_user((unsigned char*)arg,&tmp,1);
 		break;
 	default:
-		break;	
+		break;
 	}
 	return 0;
 }
@@ -274,36 +278,37 @@ static int can_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsi
 //read the head data from RXbuffer 
 static ssize_t can_read(struct file *filp,char *buf,size_t count,loff_t *f_pos)
 {
- //add your own code
-return 0;
+	//add your own code
+	//TODO 尼玛，居然还有要自己填的代码啊......===================================ATTENTION==========================================
+	return 0;
 }
 
 
 static ssize_t can_write(struct file *filp,const char *buf,size_t count,loff_t *f_pos)
 {
-  int ret=0;
+	int ret=0;
 
-   down_interruptible(&tx_mutex);
+	down_interruptible(&tx_mutex);
 
-   if(TXbuffer.count<TXBUFLEN) 
-	TXbuffer.count++;
-   else return -1;
+	if(TXbuffer.count<TXBUFLEN) 
+		TXbuffer.count++;
+	else return -1;
 
-   copy_from_user(TXbuffer.TXdata+(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN,(PCanData *)buf,16);
-   
-   printk("In can_write:TXdata tail is %d\n",TXbuffer.TXdata[(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN].dlc);
- 
-    //TXdata[0].dlc=count;
-	 
-    //Transmit data to Bus	
-    ret=can_data_send(TXbuffer.head,0);
+	copy_from_user(TXbuffer.TXdata+(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN,(PCanData *)buf,16);
 
-   TXbuffer.head=(TXbuffer.head+1)%TXBUFLEN;
-   TXbuffer.count--;
-	  
-   up(&tx_mutex);
+	printk("In can_write:TXdata tail is %d\n",TXbuffer.TXdata[(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN].dlc);
 
-   return ret;
+	//TXdata[0].dlc=count;
+
+	//Transmit data to Bus
+	ret=can_data_send(TXbuffer.head,0);
+
+	TXbuffer.head=(TXbuffer.head+1)%TXBUFLEN;
+	TXbuffer.count--;
+
+	up(&tx_mutex);
+
+	return ret;
 }
 
 
@@ -317,14 +322,14 @@ int can_release(struct inode *inode,struct file *filp)
 {
 	MOD_DEC_USE_COUNT;
 
-/********************disable mcp2510************************************************/
-        disable2510();
-/********************set mcp2510 into reset mode************************************/
-        Reset_Instr_2510();
-/********************close the interrupt of ENT1*************************************
- In case the glitch of EINT2 intervene the system.
-************************************************************************************/
-        disable_irq(IRQ_EINT2);  
+	/********************disable mcp2510************************************************/
+	disable2510();
+	/********************set mcp2510 into reset mode************************************/
+	Reset_Instr_2510();
+	/********************close the interrupt of ENT1*************************************
+	In case the glitch of EINT2 intervene the system.
+	************************************************************************************/
+	disable_irq(IRQ_EINT2);  
 
 	can_fasync(-1,filp,0);
 	return 0;
@@ -346,65 +351,66 @@ void test(void)
 
 struct file_operations candev_fops=
 {
-	read  :can_read,
-	write :can_write,
-	ioctl  :can_ioctl,
-	open :can_open,
-	release :can_release,
-	fasync :can_fasync,	
+read  :can_read,
+write :can_write,
+ioctl  :can_ioctl,
+open :can_open,
+release :can_release,
+fasync :can_fasync,	
 
 };
 
+//注：初始化驱动 insmod
 int init_module(void)
 {
 	unsigned int result; 
 	int flags;
 
-/********************set timer******************************************************/
-/*	int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
-    if(int_timer==NULL)return -1;
+	/********************set timer******************************************************/
+	/*	int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
+	if(int_timer==NULL)return -1;
 	memset(int_timer,0,sizeof(struct timer_list));
 	inttimer_register(int_timer,TIME_STEP);
-*/
+	*/
 
-/********************set irq ofr EINT2**********************************************/
-   	local_irq_save(flags);
+	/********************set irq ofr EINT2**********************************************/
+	local_irq_save(flags);
 	set_external_irq(IRQ_EINT2, EXT_FALLING_EDGE, GPIO_PULLUP_DIS);
 	disable_irq(IRQ_EINT2);
-        enable_irq(IRQ_EINT2);	
+	enable_irq(IRQ_EINT2);	
 	local_irq_restore(flags);
 	result = request_irq(IRQ_EINT2,&can_interrupt,SA_INTERRUPT,"can",NULL);
 	if (result)
 	{
-	    printk("Can't get assigned irq %d,result=%d\n",IRQ_EINT2,result);
-	    return result;
+		printk("Can't get assigned irq %d,result=%d\n",IRQ_EINT2,result);
+		return result;
 	}
 
 	devfs_register(NULL,INT_DEV_NAME,DEVFS_FL_AUTO_DEVNUM,INT_DEV_MAJOR,
-			1,S_IFCHR|S_IRUGO|S_IWUGO,&candev_fops,NULL);	
+		1,S_IFCHR|S_IRUGO|S_IWUGO,&candev_fops,NULL);
 
 	//result=register_chrdev(INT_DEV_MAJOR,INT_DEV_NAME,&candev_fops); 
 	//if(result<0)printk("insmod failed\n");
-        //else printk("insmod successfully %d\n",result);
+	//else printk("insmod successfully %d\n",result);
 
-/********************do address map from phsical address to virtual address*********/
-	address_map();	
+	/********************do address map from physical address to virtual address*********/
+	address_map();
 
-	//init the mutex semphare
+	//init the mutex semaphore
 	init_MUTEX(&rx_mutex);
 	init_MUTEX(&tx_mutex);
-        
+
 	return 0;
 }
 
-
+//注：卸载驱动 rmmmod
 void cleanup_module(void)
 {
 	disable_irq(IRQ_EINT2);
 	free_irq(IRQ_EINT2, NULL);
 
 	if(int_timer!=NULL)
-	{		
+	{
 		if(del_timer(int_timer)==1)printk("---close timer-----\n");
 		kfree(int_timer);
 	}
@@ -417,5 +423,3 @@ void cleanup_module(void)
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Luoyigui @Yuqian electronics science technology com. ltd.");
 MODULE_DESCRIPTION("CAN Module");
-
-
--- src/mcp2510/can_sensor.c
+++ src/mcp2510/can_sensor.c
@@ -10,7 +10,7 @@
 #include<linux/devfs_fs_kernel.h> 
 #include<asm/io.h>
 #include<asm/uaccess.h>
-  
+
 #include<linux/time.h>
 #include<linux/timer.h>
 
@@ -47,50 +47,51 @@ void inttimer_register(struct timer_list *pdata,unsigned long timeover);
 //interrupt handler for cam2 disconnect
 
 //triggered the interrupt on rise edge.
+//注：我不知道这是干什么的...
 void pwr1_dic_interrupt(int irq,void *d,struct pt_regs *regs)
 {	
 	unsigned char buffer,tmp;
 
 	disable_irq(IRQ_EINT2);
-       
-        //kill signal sigio to application
+
+	//kill signal SIGIO to application
 
 	//because it is in local ,there is no intervene	
-	
-	SRCPND &= (~0x00000002);    
-   	INTPND = INTPND;
-	
-	enable_irq(IRQ_EINT2);	   			
 
+	SRCPND &= (~0x00000002);
+	INTPND = INTPND;
+
+	enable_irq(IRQ_EINT2);
 }
 
+//注：应该是timer计时结束后进行的操作
 void inttimer_timeover(unsigned long arg)
 {
-	
 	//unsigned char buffer;
 	printk("\n===============In timer--ing==================\n");
 
-//	buffer = ReadStatus_Instr_2510();
+	//	buffer = ReadStatus_Instr_2510();
 
-//	printk("Now can state is %x\n",buffer);
-	
-/*	if((buffer&RX0IF)>0)
+	//	printk("Now can state is %x\n",buffer);
+
+	/*	if((buffer&RX0IF)>0)
 	{
-		 can_data_receive(0);
-		BitModify_Instr_2510(CANINTF, RX0IF, 0);
-                kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
+	can_data_receive(0);
+	BitModify_Instr_2510(CANINTF, RX0IF, 0);
+	kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
 	}
 	else if((buffer&RX1IF)>0)
 	{
-		 can_data_receive(1);
-		BitModify_Instr_2510(CANINTF, RX1IF, 0);
-                kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
+	can_data_receive(1);
+	BitModify_Instr_2510(CANINTF, RX1IF, 0);
+	kill_fasync(&can_async_queue,SIGIO,POLL_OUT);
 	}
-*/
+	*/
 	inttimer_register(int_timer,TIME_STEP);
-	      
+
 }
 
+//注：应该是timer的注册
 void inttimer_register(struct timer_list *pdata,unsigned long timeover)
 {
 	init_timer(pdata);
@@ -100,60 +101,65 @@ void inttimer_register(struct timer_list *pdata,unsigned long timeover)
 	add_timer(pdata);
 }
 
+/************************************************************************/
+/* 以下各个函数都是为了下边的这个can_sensor_dev_fops准备的              */
+/************************************************************************/
 
+//注：是CAN总线的fops里需要定义好的函数，可是这个can_sensor.c是所谓的"CAN sensor manager Module"
+//在can_sensor_dev_fops里只有can_sensor_open，所以会不会调用这个函数呢？不好说
 int can_open(struct inode *inode,struct file *filp)
 {
 	int flags;
 
-	MOD_INC_USE_COUNT;	
+	MOD_INC_USE_COUNT;
 
 	local_irq_save(flags);
 	Init_SPI();
 	Init_MCP2510();
 	local_irq_restore(flags);
 
-       /********************set timer******************************************************/
-        int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
-    if(int_timer==NULL)return -1;
-        memset(int_timer,0,sizeof(struct timer_list));
-        inttimer_register(int_timer,TIME_STEP);
+	/********************set timer******************************************************/
+	int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
+	if(int_timer==NULL)return -1;
+	memset(int_timer,0,sizeof(struct timer_list));
+	inttimer_register(int_timer,TIME_STEP);
 
-    return 0;
+	return 0;
 }
 
 static int can_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsigned long arg)
 {
 	unsigned char tmp;
-	
+
 	switch(cmd)
 	{
 	case IOCTL_MOD_SET:
 		copy_from_user(&tmp,(unsigned char *)arg,1);
 		printk("============IOCTL MODE SET===============\n");
 		switch(tmp){
-			case OP_NORMAL:
-				MCP2510_SetNormalMode();
-				return 1;
-			case OP_LISTEN_ONLY:
-				MCP2510_SetListenOnlyMode();
-				return 1;
-			case OP_LOOPBACK:
-				MCP2510_SetLoopbackMode();
-				return 1;
-			case OP_SLEEP:
-				MCP2510_SetSleepMode();
-				return 1;
-			case OP_CONFIG:
-				MCP2510_SetConfigMode();
-				return 1;
-			default:
-				return -1;
+		case OP_NORMAL:
+			MCP2510_SetNormalMode();
+			return 1;
+		case OP_LISTEN_ONLY:
+			MCP2510_SetListenOnlyMode();
+			return 1;
+		case OP_LOOPBACK:
+			MCP2510_SetLoopbackMode();
+			return 1;
+		case OP_SLEEP:
+			MCP2510_SetSleepMode();
+			return 1;
+		case OP_CONFIG:
+			MCP2510_SetConfigMode();
+			return 1;
+		default:
+			return -1;
 		}
 		break;
 	case IOCTL_GET_MODE:
-                if(!arg)
+		if(!arg)
 			return -1;
-	
+
 		printk("============IOCTL GET MODE===============\n");
 		tmp= Read_Instr_2510(CANSTAT);
 		tmp=((tmp&0xe0)>>1);
@@ -161,7 +167,7 @@ static int can_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsi
 		break;
 	case IOCTL_GET_CANSTAT:
 		if(!arg)
-                        return -1;
+			return -1;
 
 		printk("============IOCTL GET CANSTAT===============\n");
 		tmp= ReadStatus_Instr_2510();
@@ -175,17 +181,17 @@ static int can_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsi
 
 static ssize_t can_read(struct file *filp,char *buf,size_t count,loff_t *f_pos)
 {
- ssize_t ret=0;
+	ssize_t ret=0;
 
 	down_interruptible(&rx_mutex);
-    
-        if(RXbuffer.count>0) //valid message
+
+	if(RXbuffer.count>0) //valid message
 	{
 		copy_to_user(buf,(char *)(RXbuffer.RXdata+RXbuffer.head),16);			
 		RXbuffer.head=(RXbuffer.head+1)%RXBUFLEN;
 		RXbuffer.count--;
 		ret=16;
-        }
+	}
 	else 
 	{
 		ret=-1;;
@@ -199,29 +205,29 @@ static ssize_t can_read(struct file *filp,char *buf,size_t count,loff_t *f_pos)
 
 static ssize_t can_write(struct file *filp,const char *buf,size_t count,loff_t *f_pos)
 {
-  int ret=0;
+	int ret=0;
 
-   down_interruptible(&tx_mutex);
+	down_interruptible(&tx_mutex);
 
-   if(TXbuffer.count<TXBUFLEN) 
-	TXbuffer.count++;
-   else return -1;
+	if(TXbuffer.count<TXBUFLEN) 
+		TXbuffer.count++;
+	else return -1;
 
-   copy_from_user(TXbuffer.TXdata+(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN,(PCanData *)buf,16);
-   
-   printk("In can_write:TXdata tail is %d\n",TXbuffer.TXdata[(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN].dlc);
- 
-    //TXdata[0].dlc=count;
-	 
-    //Transmit data to Bus	
-    ret=can_data_send(TXbuffer.head,0);
+	copy_from_user(TXbuffer.TXdata+(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN,(PCanData *)buf,16);
 
-   TXbuffer.head=(TXbuffer.head+1)%TXBUFLEN;
-   TXbuffer.count--;
-	  
-   up(&tx_mutex);
+	printk("In can_write:TXdata tail is %d\n",TXbuffer.TXdata[(TXbuffer.head+TXbuffer.count-1)%TXBUFLEN].dlc);
 
-   return ret;
+	//TXdata[0].dlc=count;
+
+	//Transmit data to Bus	
+	ret=can_data_send(TXbuffer.head,0);
+
+	TXbuffer.head=(TXbuffer.head+1)%TXBUFLEN;
+	TXbuffer.count--;
+
+	up(&tx_mutex);
+
+	return ret;
 }
 
 
@@ -235,14 +241,14 @@ int can_release(struct inode *inode,struct file *filp)
 {
 	MOD_DEC_USE_COUNT;
 
-/********************disable mcp2510************************************************/
-        disable2510();
-/********************set mcp2510 into reset mode************************************/
-        Reset_Instr_2510();
-/********************close the interrupt of ENT2*************************************
- In case the glitch of EINT2 intervene the system.
-************************************************************************************/
-        disable_irq(IRQ_EINT2);  
+	/********************disable mcp2510************************************************/
+	disable2510();
+	/********************set mcp2510 into reset mode************************************/
+	Reset_Instr_2510();
+	/********************close the interrupt of ENT2*************************************
+	In case the glitch of EINT2 intervene the system.
+	************************************************************************************/
+	disable_irq(IRQ_EINT2);  
 
 	can_fasync(-1,filp,0);
 	return 0;
@@ -261,61 +267,65 @@ void test(void)
 	printk("\n====buffer %x======\n",buffer);
 }
 
+/************************************************************************/
+/* 以上各个函数都是为了下边的这个can_sensor_dev_fops准备的              */
+/************************************************************************/
 
 struct file_operations can_sensor_dev_fops=
 {
-	read  :can_sensor_read,
-	write :can_sensor_write,
-	ioctl  :can_sensor_ioctl,
-	open :can_sensor_open,
-	release :can_sensor_release,
-	fasync :can_sensor_fasync,	
+read  :can_sensor_read,
+write :can_sensor_write,
+ioctl  :can_sensor_ioctl,
+open :can_sensor_open,
+release :can_sensor_release,
+fasync :can_sensor_fasync,	
 
 };
 
+//注：初始化module，insmod时
 int init_module(void)
 {
 	unsigned int result; 
 	int flags;
 
-/********************set timer******************************************************/
-/*	int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
-    if(int_timer==NULL)return -1;
+	/********************set timer******************************************************/
+	/*	int_timer=kmalloc(sizeof(struct timer_list),GFP_KERNEL);
+	if(int_timer==NULL)return -1;
 	memset(int_timer,0,sizeof(struct timer_list));
 	inttimer_register(int_timer,TIME_STEP);
-*/
+	*/
 
-/********************set irq ofr EINT2**********************************************/
-   	local_irq_save(flags);
+	/********************set irq ofr EINT2**********************************************/
+	local_irq_save(flags);
 	set_external_irq(IRQ_EINT2, EXT_FALLING_EDGE, GPIO_PULLUP_DIS);
 	disable_irq(IRQ_EINT2);
-        enable_irq(IRQ_EINT2);	
+	enable_irq(IRQ_EINT2);	
 	local_irq_restore(flags);
 	result = request_irq(IRQ_EINT2,&can_interrupt,SA_INTERRUPT,"can",NULL);
 	if (result)
 	{
-	    printk("Can't get assigned irq %d,result=%d\n",IRQ_EINT2,result);
-	    return result;
+		printk("Can't get assigned irq %d,result=%d\n",IRQ_EINT2,result);
+		return result;
 	}
 
 	devfs_register(NULL,INT_DEV_NAME,DEVFS_FL_AUTO_DEVNUM,INT_DEV_MAJOR,
-			1,S_IFCHR|S_IRUGO|S_IWUGO,&candev_fops,NULL);	
+		1,S_IFCHR|S_IRUGO|S_IWUGO,&candev_fops,NULL);	
 
 	//result=register_chrdev(INT_DEV_MAJOR,INT_DEV_NAME,&candev_fops); 
 	//if(result<0)printk("insmod failed\n");
-        //else printk("insmod successfully %d\n",result);
+	//else printk("insmod successfully %d\n",result);
 
-/********************do address map from phsical address to virtual address*********/
+	/********************do address map from physical address to virtual address*********/
 	address_map();	
 
-	//init the mutex semphare
+	//init the mutex semaphore
 	init_MUTEX(&rx_mutex);
 	init_MUTEX(&tx_mutex);
-        
+
 	return 0;
 }
 
-
+//注：清理掉，rmmod
 void cleanup_module(void)
 {
 	disable_irq(IRQ_EINT2);
@@ -335,5 +345,3 @@ void cleanup_module(void)
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Luoyigui @Yuqian electronics science technology com. ltd.");
 MODULE_DESCRIPTION("CAN sensor manager Module");
-
-
